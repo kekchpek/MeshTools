937b35826387b62189007042a5e87492fd21f089
Nikolai <nikolai.selivanov@productmadness.com>
2022-08-09 20:05:11 +0300

    Add cut confirmation in edit mode

--- Assets/MeshTools/MeshKnife/Editor/MeshKnifeEditor.cs
+++ Assets/MeshTools/MeshKnife/Editor/MeshKnifeEditor.cs
@@ -1,20 +1,27 @@
-﻿using MeshTools.MeshKnife;
+﻿using System;
 using UnityEditor;
 using UnityEngine;
 
-namespace MeshTools.Editor.MeshKnife
+namespace MeshTools.MeshKnife.Editor
 {
-    [CustomEditor(typeof(MeshTools.MeshKnife.MeshKnife))]
+    [CustomEditor(typeof(global::MeshTools.MeshTools.MeshKnife.MeshKnife))]
     [CanEditMultipleObjects]
     public class MeshKnifeEditor : UnityEditor.Editor
     {
+
+        private const string AskCutConfirmationInEditModeKey = nameof(AskCutConfirmationInEditModeKey);
+        
         private static IMeshKnife _target;
+
+        private bool _askCutConfirmationInEditMode;
         
         private void Awake()
         {
             _target = target as IMeshKnife;
             if (_target == null)
                 Debug.LogError($"Target is not a {nameof(MeshTools.MeshKnife.MeshKnife)}");
+            
+            _askCutConfirmationInEditMode = Convert.ToBoolean(PlayerPrefs.GetInt(AskCutConfirmationInEditModeKey, 1));
         }
 
         public override void OnInspectorGUI()
@@ -27,9 +34,29 @@ namespace MeshTools.Editor.MeshKnife
                 _target.Initialize();
             }
 
+            var prevVal = _askCutConfirmationInEditMode;
+            _askCutConfirmationInEditMode =
+                GUILayout.Toggle(_askCutConfirmationInEditMode, "Ask confirmation in EditMode");
+
+            if (_askCutConfirmationInEditMode != prevVal)
+            {
+                PlayerPrefs.SetInt(AskCutConfirmationInEditModeKey, Convert.ToInt32(_askCutConfirmationInEditMode));
+            }
+
             if (_target.Initialized && GUILayout.Button("Cut"))
             {
-                _target.Cut();
+                if (_askCutConfirmationInEditMode)
+                {
+                    if (EditorUtility.DisplayDialog("Confirm cut in edit mode", 
+                        "Are you sure you want to cut a mesh in edit mode?", "Cut", "Cancel"))
+                    {
+                        _target.Cut();
+                    }
+                }
+                else
+                {
+                    _target.Cut();
+                }
             }
 
             serializedObject.ApplyModifiedProperties();
